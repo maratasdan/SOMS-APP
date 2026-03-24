c30c1bc789d670fe142408005567800468fc7127
maratasdan <maratas.094747@gensan.sti.edu.ph>
2026-03-24 16:58:13 +0800

    Approval for confirming bins

---
+++ SOMS/DR/ApproveBins.swift
@@ -0,0 +1,18 @@
+//
+//  ApproveBins.swift
+//  SOMS
+//
+//  Created by Dan on 3/24/26.
+//
+
+import SwiftUI
+
+struct ApproveBins: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApproveBins()
+}
---
+++ SOMS/DR/ApproveBinsConfirm.swift
@@ -0,0 +1,18 @@
+//
+//  ApproveBinsConfirm.swift
+//  SOMS
+//
+//  Created by Dan on 3/24/26.
+//
+
+import SwiftUI
+
+struct ApproveBinsConfirm: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApproveBinsConfirm()
+}
